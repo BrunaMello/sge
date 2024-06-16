086daade2b932ccfe8001798ce5158a43ccdeb5c
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-06-16 10:39:27 +0100

    012 - Modelagem de categorias e fornecedores

---
+++ suppliers/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Supplier(models.Model):
+	name = models.CharField(max_length=500)
+	description = models.TextField(null=True, blank=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	# Sort Pattern
+
+	class Meta:
+		ordering = ['name']
+
+	def __str__(self):
+		return self.name
