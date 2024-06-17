0a7f021801b2d362e705729e5080418e705b26ae
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-06-17 19:51:01 +0100

    025 - Estilizando form de brands

--- brands/forms.py
+++ brands/forms.py
@@ -7,3 +7,11 @@ class BrandForm(forms.ModelForm):
 	class Meta:
 		model = models.Brand
 		fields = ['name', 'description']
+		widgets = {
+			'name': forms.TextInput({'class': 'form-control'}),
+			'description': forms.Textarea({'class': 'form-control', 'rows': 3})
+		}
+		labels = {
+			'name': 'Name',
+			'description': 'Description'
+		}
