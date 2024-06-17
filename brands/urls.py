24ab6da443b6b27efbbef3f1654c6c67b74059c9
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-06-17 18:22:32 +0100

    020 - Validando as camadas de brands

---
+++ brands/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('brands/list/', views.BrandListView.as_view(), name='brand-list')
+
+]
--- brands/views.py
+++ brands/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from . import models
+
+
+class BrandListView(ListView):
+	model = models.Brand
+	template_name = 'brand_list.html'
+	context_object_name = 'brands'
