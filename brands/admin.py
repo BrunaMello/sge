95206efd26e1d78c7d0b0237fc23aebde3d419b8
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-06-17 18:27:14 +0100

    021 - Admin de brands

---
+++ brands/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from . import models
+
+
+class BrandAdmin(admin.ModelAdmin):
+	list_display = ('name', 'description',)
+	search_fields = ('name',)
+
+
+admin.site.register(models.Brand, BrandAdmin)
