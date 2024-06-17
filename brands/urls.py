81b6495b6288b31fc7696d9520d24a044f59481c
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-06-17 20:17:54 +0100

    028 - Update de brands

--- brands/urls.py
+++ brands/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
 	path('brands/list/', views.BrandListView.as_view(), name='brand_list'),
 	path('brands/create/', views.BrandCreateView.as_view(), name='brand_create'),
-	path('brands/<int:pk>/detail/', views.BrandDetailView.as_view(), name='brand_detail')
+	path('brands/<int:pk>/detail/', views.BrandDetailView.as_view(), name='brand_detail'),
+	path('brands/<int:pk>/update/', views.BrandUpdateView.as_view(), name='brand_update'),
 
 ]
--- brands/views.py
+++ brands/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 
 from . import models, forms
 
@@ -29,3 +29,10 @@ class BrandCreateView(CreateView):
 class BrandDetailView(DetailView):
 	model = models.Brand
 	template_name = 'brand_detail.html'
+
+
+class BrandUpdateView(UpdateView):
+	model = models.Brand
+	template_name = 'brand_update.html'
+	success_url = reverse_lazy('brand_list')
+	form_class = forms.BrandForm
