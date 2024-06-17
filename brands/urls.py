baeaa3add3cea8de85366910f97fd1e82080dfb8
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-06-17 18:45:49 +0100

    023 - Implementando filtros de brands

--- brands/urls.py
+++ brands/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('brands/list/', views.BrandListView.as_view(), name='brand-list')
+	path('brands/list/', views.BrandListView.as_view(), name='brand_list')
 
 ]
