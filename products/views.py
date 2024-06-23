da7723b793433e715528e0a089da7bb31391974d
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-06-23 09:34:47 +0100

    037 - Filtros de produtos (parte 2)

--- products/views.py
+++ products/views.py
@@ -13,9 +13,21 @@ class ProductListView(ListView):
 	def get_queryset(self):
 		queryset = super().get_queryset()
 		title = self.request.GET.get('title')
+		serie_number = self.request.GET.get('serie_number')
+		category = self.request.GET.get('category')
+		brand = self.request.GET.get('brand')
 
 		if title:
 			queryset = queryset.filter(title__icontains=title)
+
+		if serie_number:
+			queryset = queryset.filter(serie_number__icontains=serie_number)
+
+		if category:
+			queryset = queryset.filter(category__id=category)
+
+		if brand:
+			queryset = queryset.filter(brand__id=brand)
 
 		return queryset
 
