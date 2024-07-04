9eb60d7fc7adbdac63c74a8afd7ee62c8a2153c8
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-07-04 18:17:12 +0100

    038 - Inventário automático de produtos (parte 1)

--- inflows/apps.py
+++ inflows/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class InflowsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'inflows'
+	default_auto_field = 'django.db.models.BigAutoField'
+	name = 'inflows'
+
+	def ready(self):
+		import inflows.signals
+		signals = inflows.signals
