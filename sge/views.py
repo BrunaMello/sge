cda70b6c5b72c3e9b3604643b4def1dd6b675f1e
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-07-08 20:42:55 +0100

    042 - Métricas de produtos no contexto

--- sge/views.py
+++ sge/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 
 
 def home(request):
-	context = {}
+	product_metrics = {
+		'total_cost_price': 100000,
+		'total_selling_price': 200000,
+		'total_quantity': 1000,
+		'total_profit': 100000,
+	}
+
+	context = {
+		'product_metrics': product_metrics,
+	}
+
 	return render(request, 'home.html', context)
