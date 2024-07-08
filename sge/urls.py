1ae2a658819333a50341fea045f5b8717e26b601
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-07-08 20:26:21 +0100

    040 - Estrutura inicial do dashboard

--- sge/urls.py
+++ sge/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
 	path('admin/', admin.site.urls),
+
+	path('', views.home, name='home'),
+
 	path('', include('brands.urls')),
 	path('', include('categories.urls')),
 	path('', include('suppliers.urls')),
