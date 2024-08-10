c2ff0765bf6aa3b8f90e1989f6eced1d1b09abb2
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-08-10 10:14:43 +0100

    046 - Criando métricas de vendas

--- sge/views.py
+++ sge/views.py
@@ -5,9 +5,11 @@ from . import metrics
 
 def home(request):
 	product_metrics = metrics.get_product_metrics()
+	sales_metrics = metrics.get_sales_metrics()
 
 	context = {
 		'product_metrics': product_metrics,
+		'sales_metrics': sales_metrics,
 	}
 
 	return render(request, 'home.html', context)
