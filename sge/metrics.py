ae78ca8c3a13962ad54777f5203071eb731fb950
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-08-26 21:18:30 +0100

    049 - Gráficos de vendas (parte 2) usando o chart.js

--- sge/metrics.py
+++ sge/metrics.py
@@ -1,4 +1,5 @@
-from django.db.models import Sum
+from django.db.models import Sum, F
+from django.utils import timezone
 from django.utils.formats import number_format
 
 from outflows.models import Outflow
@@ -35,4 +36,23 @@ def get_sales_metrics():
 		total_products_sold=total_products_sold,
 		total_sales_value=number_format(total_sales_value, decimal_pos=2, force_grouping=True),
 		total_sales_profit=number_format(total_sales_profit, decimal_pos=2, force_grouping=True),
+	)
+
+
+def get_daily_sales_data():
+	today = timezone.now().date()
+	dates = [str(today - timezone.timedelta(days=i)) for i in range(6, -1, -1)]
+	values = list()
+
+	for date in dates:
+		sales_total = Outflow.objects.filter(
+			created_at__date=date
+		).aggregate(
+			total_sales=Sum(F('product__selling_price') * F('quantity'))
+		)['total_sales'] or 0
+		values.append(float(sales_total))
+
+	return dict(
+		dates=dates,
+		values=values,
 	)
--- sge/views.py
+++ sge/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 
 from . import metrics
@@ -6,10 +8,12 @@ from . import metrics
 def home(request):
 	product_metrics = metrics.get_product_metrics()
 	sales_metrics = metrics.get_sales_metrics()
+	daily_sales_data = metrics.get_daily_sales_data()
 
 	context = {
 		'product_metrics': product_metrics,
 		'sales_metrics': sales_metrics,
+		'daily_sales_data': json.dumps(daily_sales_data),
 	}
 
 	return render(request, 'home.html', context)
